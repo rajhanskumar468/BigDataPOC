8b2b63ff27fabdfb56aa44bc5f044138461255e5
EMIRATES\S787968 <rajhans.kumar@dnata.com>
2020-04-29 11:06:29 +0400

    adding POC

---
+++ BigData/sparkSetup.bat
@@ -0,0 +1,5 @@
+set PATH=C:\Rajhans\BigData\scala-2.12.8\bin;C:\Rajhans\BigData\spark-2.4.0-bin-hadoop2.7\bin;C:\Rajhans\BigData\sbt\bin;%path%;
+set HADOOP_HOME=C:\Rajhans\BigData\winutils-master\hadoop-2.7.1
+
+cmd
+C:\Rajhans\BigData\winutils-master\bin\winutils.exe
